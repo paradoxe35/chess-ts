1ca087a36e54204fc7da3abb71955e83c2056595
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-01-25 00:01:42 +0000

    tiny improvements

--- src/utils/online.ts
+++ src/utils/online.ts
@@ -149,6 +149,7 @@ export function useOnlinePlayer() {
         try {
           peer.current?.disconnect();
           conn.close();
+          peer.current = null;
         } catch (_) {}
 
         actorRef.current.send({
