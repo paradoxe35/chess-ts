721c28dfb70c7536406130a8da87419018f82d12
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-01-26 11:00:40 +0000

    tiny changes

--- src/components/settings/activities.tsx
+++ src/components/settings/activities.tsx
@@ -98,7 +98,7 @@ function ChatNotifications(props: PropsWithChildren<{ selected: boolean }>) {
       onClick={() => setNotification(false)}
     >
       {notification && (
-        <span className="-top-1 start-4 absolute w-3 h-3 bg-green-500 border-2 border-white dark:border-gray-800 rounded-full"></span>
+        <span className="-top-1 start-4 absolute w-3 h-3 bg-green-500 border-2 border-white rounded-full"></span>
       )}
 
       {props.children}
